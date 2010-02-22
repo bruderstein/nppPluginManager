5c8dda59fb97302567f9ab09752a25e799e65e84
Dave Brotherstone <davegb@pobox.com>
2010-02-22 20:01:20 +0000

    Fix Win7 / Vista compatibility with UAC
      - Add manifest to GPUP to require administrator
      - Change StartGpup method to use ShellExecute instead of CreateProcess

--- pluginManager/src/Utility.cpp
+++ pluginManager/src/Utility.cpp
@@ -1,7 +1,7 @@
 /*
 This file is part of Plugin Manager Plugin for Notepad++
 
-Copyright (C)2009 Dave Brotherstone <davegb@pobox.com>
+Copyright (C)2009-2010 Dave Brotherstone <davegb@pobox.com>
 
 This program is free software; you can redistribute it and/or
 modify it under the terms of the GNU General Public License
@@ -81,8 +81,8 @@ void Utility::startGpup(HWND errorParent, const TCHAR *nppDir, const TCHAR *argu
 		return;
 	}
 
-	gpupExe.insert(0, _T("\""));
-	gpupExe.append(_T("\" "));
+	//gpupExe.insert(0, _T("\""));
+	//gpupExe.append(_T("\" "));
 
 	tstring gpupArguments(arguments);
 
@@ -112,13 +112,23 @@ void Utility::startGpup(HWND errorParent, const TCHAR *nppDir, const TCHAR *argu
 	gpupArguments.append(notepadExe);
 	gpupArguments.append(_T("\""));
 	
-	gpupExe.append(gpupArguments);
+	// gpupExe.append(gpupArguments);
+	/* 
 	STARTUPINFO startup;
 	memset(&startup, 0, sizeof(STARTUPINFO));
 
 	startup.cb = sizeof(STARTUPINFO);
+
 	PROCESS_INFORMATION procinfo;
-	
+*/
+	::ShellExecute(NULL,
+				   _T("open"),
+				   gpupExe.c_str(),
+				   gpupArguments.c_str(),
+				   NULL,
+				   SW_SHOW);
+
+	/*
     ::CreateProcess(NULL,						 // No module - using command line
 				    (TCHAR *)gpupExe.c_str(),    // command line & arguments
 						NULL,        // process security
@@ -129,5 +139,5 @@ void Utility::startGpup(HWND errorParent, const TCHAR *nppDir, const TCHAR *argu
 						NULL,        // inherit directory
 						&startup,    // STARTUPINFO
 						&procinfo);  // PROCESS_INFORMATION
-
+*/
 }
