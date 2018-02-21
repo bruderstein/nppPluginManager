1a02cf1e8a1fcc8f7f4d2528f45d0270c3b5a6a6
Christian Grasser <christian.grasser@live.de>
2018-02-21 21:48:50 +0100

    correct review comment from PR #87

--- TinyXml/src/tinyxmlparser.cpp
+++ TinyXml/src/tinyxmlparser.cpp
@@ -334,7 +334,7 @@ const TCHAR* TiXmlBase::ReadText(	const TCHAR* p,
 		while (	   p && *p
 				&& !StringEqual( p, endTag, caseInsensitive ) )
 		{
-			if ( *p == L'\r' || *p == L'\n' )
+			if ( *p == TEXT('\r') || *p == TEXT('\n') )
 			{
 				whitespace = true;
 				++p;
